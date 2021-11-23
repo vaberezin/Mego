3f07630242e21c50e566bafefe3284cf44cab9d6
Vlad Berezin <vladislav.berezin@amtrustgroup.comgit config --listgit config --global user.email vladislav.berezin@amtrustgroup.com>
2021-11-23 13:28:26 +0300

    DI to controller, add comments

--- Mego/Controllers/SearchController.cs
+++ Mego/Controllers/SearchController.cs
@@ -10,22 +10,33 @@ using Mego.Models;
 
 namespace Mego.Controllers
 {
-    public class SearchController : Controller
+
+    [ApiController]    
+    [Route("[controller]/[action]")]
+    public class SearchDataController : Controller
     {
+        DataManagement dataManagement;
+        public SearchDataController(DataManagement _data)
+        {
+            this.dataManagement = _data;
+        }
         SearchSystem ExternalA;
         SearchSystem ExternalB;
         SearchSystem ExternalC;
         SearchSystem ExternalD;
 
         
-
-        // GET: SearchController/Details/5
+        [HttpGet]
+        // GET: Search/Search?wait=X&randomMin=Y&randomMax=Z
         public ActionResult Search(int wait, int randomMin, int randomMax)
         {            
             ExternalA = new SearchSystem(randomMin, randomMax);
             ExternalB = new SearchSystem(randomMin, randomMax);
             ExternalC = new SearchSystem(randomMin, randomMax);
             ExternalD = new SearchSystem(randomMin, randomMax);
+
+            //Create list of objects and send it to client
+            List<SearchEngineModel>  resultList = new List<SearchEngineModel>();
 
             CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
             
@@ -39,14 +50,33 @@ namespace Mego.Controllers
             SearchEngineModel AResult = A.Result;
             SearchEngineModel BResult = B.Result;
             SearchEngineModel CResult = C.Result;
+            SearchEngineModel DResult;
+
 
             if (CResult.SearchResult == "OK")
             {
-                D.Start();
+                Task.WhenAll(new Task[]{D});
+                DResult = D.Result;
+                resultList.AddRange(new SearchEngineModel[]{AResult,BResult,CResult,DResult});
             }
-            return View();
+            else
+            {
+                resultList.AddRange(new SearchEngineModel[]{AResult,BResult,CResult});
+            }
+            
+            //Add searchResults to DataManagement object (serves as a warehouse here).
+            dataManagement.warehouse.AddRange(resultList);
+
+            return Json(resultList);
         }
 
+        [HttpGet]
+        public ActionResult Metrics(){  //Report action
+            
+            var Result = dataManagement.warehouse.GroupBy(d => (Math.Ceiling(Convert.ToDecimal(d.Performance/1000)),d.SearchEngineName)).ToList(); //test math.ceiling
+
+            return Json(Result);
+        }
         
     }
 }
