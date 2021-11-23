1ad2e1db69c277451d81b41965979a0831f8a301
Vlad Berezin <vladislav.berezin@amtrustgroup.comgit config --listgit config --global user.email vladislav.berezin@amtrustgroup.com>
2021-11-23 13:27:17 +0300

    Add DataManagement model

---
+++ Mego/BaseClasses/DataManagement.cs
@@ -0,0 +1,10 @@
+using Mego.Models;
+using System.Collections.Generic;
+
+namespace Mego.BaseClasses
+{
+    public class DataManagement
+    {
+        public List<SearchEngineModel> warehouse = new List<SearchEngineModel>();        
+    }
+}
